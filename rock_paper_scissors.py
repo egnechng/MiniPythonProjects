28e9978e8beefda4f6b1d6343b95d59b76564976
Eugene Chang <90797215+eug-chang@users.noreply.github.com>
2023-05-27 00:31:50 -0400

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,51 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Enter Rock/Paper/Scissors or q to quit. ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        print("Invalid input!")
+        continue
+
+    random_number = random.randint(0, 2)
+    # 0-rock, 1-paper, 2-scissors
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You win!")
+        user_wins += 1
+        continue
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You win!")
+        user_wins += 1
+        continue
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You win!")
+        user_wins += 1
+        continue
+
+    elif user_input == computer_pick:
+        print("Tie!")
+        continue
+
+    else:
+        print("You lose!")
+        computer_wins += 1
+
+print("You won", str(user_wins), "times")
+
+print("Computer won", str(computer_wins), "times")
+
+
+
+        
